3af92f842427154d48f6d2992470f99005f0ebe3
Zayn Patel <65105835+zaynpatel@users.noreply.github.com>
2023-12-07 14:37:56 -0500

    Upload jetson nano shell commands for camera

---
+++ nano.zsh
@@ -0,0 +1,6 @@
+lsusb
+v4l2-ctl – list-devices (confirm that we see the /dev/video0
+gst-launch-1.0 v4l2src device=/dev/video0 ! 'image/jpeg,width=640,height=480,framerate=30/1' ! jpegdec ! xvimagesink
+gst-launch-1.0 v4l2src device=/dev/video0 ! 'image/jpeg,width=640,height=480,framerate=30/1' ! jpegdec ! videoflip method=rotate-180 ! xvimagesink
+gst-launch-1.0 v4l2src device=/dev/video0 num-buffers=1 ! 'image/jpeg,width=640,height=480,framerate=30/1' ! filesink location=snapshot.jpg
+gst-launch-1.0 v4l2src device=/dev/video0 num-buffers=1 ! 'image/jpeg,width=640,height=480,framerate=30/1' ! jpegdec ! videoflip method=rotate-180 ! jpegenc ! filesink location=flipped_snapshot.jpg
