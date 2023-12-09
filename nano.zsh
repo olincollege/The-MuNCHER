abded890c19b4bc7c761b704f61d7ea2ebe18a02
Zayn Patel <65105835+zaynpatel@users.noreply.github.com>
2023-12-09 17:36:54 -0500

    Fix small error with list devices command

--- nano.zsh
+++ nano.zsh
@@ -1,5 +1,5 @@
 lsusb
-v4l2-ctl – list-devices (confirm that we see the /dev/video0
+v4l2-ctl --list-devices (confirm that we see the /dev/video0
 gst-launch-1.0 v4l2src device=/dev/video0 ! 'image/jpeg,width=640,height=480,framerate=30/1' ! jpegdec ! xvimagesink
 gst-launch-1.0 v4l2src device=/dev/video0 ! 'image/jpeg,width=640,height=480,framerate=30/1' ! jpegdec ! videoflip method=rotate-180 ! xvimagesink
 gst-launch-1.0 v4l2src device=/dev/video0 num-buffers=1 ! 'image/jpeg,width=640,height=480,framerate=30/1' ! filesink location=snapshot.jpg
